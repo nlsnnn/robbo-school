3bc76db5595d906ccda8b28fd5071943b119a146
nlsnnn <egor.larrr@gmail.com>
2025-11-23 23:38:51 +0300

    feat: initialize frontend with Vite, React, and Tailwind CSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter } from 'react-router-dom'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+    </BrowserRouter>
+  )
+}
+
+export default App
