3756c75763dc1222e4eed8b8c02e161b3d2c6fcc
nlsnnn <egor.larrr@gmail.com>
2025-11-23 21:52:48 +0300

    feat: implement course CRUD methods with error handling and context support

--- api/internal/storage/postgresql/postgresql.go
+++ api/internal/storage/postgresql/postgresql.go
@@ -1,8 +1,12 @@
 package postgresql
 
 import (
-	"api/internal/config"
+	"context"
+	"database/sql"
 	"fmt"
+
+	"api/internal/config"
+	"api/internal/storage"
 
 	_ "github.com/jackc/pgx/stdlib"
 	"github.com/jmoiron/sqlx"
@@ -34,4 +38,75 @@ func New(dbConfig config.Db) (*Storage, error) {
 
 func (s *Storage) Close() error {
 	return s.db.Close()
+}
+
+func (s *Storage) SaveCourse(ctx context.Context, course storage.Course) (storage.Course, error) {
+	query := "INSERT INTO courses (title, description) VALUES (:title, :description) RETURNING id, created_at"
+
+	rows, err := s.db.NamedQueryContext(ctx, query, course)
+	if err != nil {
+		return storage.Course{}, fmt.Errorf("sql insert error: %w", err)
+	}
+	defer rows.Close()
+
+	if rows.Next() {
+		if err := rows.Scan(&course.Id, &course.CreatedAt); err != nil {
+			return storage.Course{}, fmt.Errorf("scan error: %w", err)
+		}
+		return course, nil
+	}
+
+	return storage.Course{}, fmt.Errorf("no rows returned")
+}
+
+func (s *Storage) GetCourses(ctx context.Context) ([]storage.Course, error) {
+	courses := []storage.Course{}
+	err := s.db.SelectContext(ctx, &courses, "SELECT * FROM courses")
+	if err != nil {
+		return []storage.Course{}, fmt.Errorf("sql select error: %w", err)
+	}
+
+	return courses, nil
+}
+
+func (s *Storage) GetCourseByID(ctx context.Context, id int) (storage.Course, error) {
+	course := storage.Course{}
+	err := s.db.GetContext(ctx, &course, "SELECT * FROM courses WHERE id = $1", id)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return storage.Course{}, storage.ErrCourseNotFound
+		}
+		return storage.Course{}, fmt.Errorf("sql get error: %w", err)
+	}
+
+	return course, nil
+}
+
+func (s *Storage) UpdateCourse(ctx context.Context, id int, course storage.Course) (storage.Course, error) {
+	query := "UPDATE courses SET title = :title, description = :description WHERE id = :id RETURNING id, created_at"
+
+	course.Id = id
+	rows, err := s.db.NamedQueryContext(ctx, query, course)
+	if err != nil {
+		return storage.Course{}, fmt.Errorf("sql update error: %w", err)
+	}
+	defer rows.Close()
+
+	if rows.Next() {
+		if err := rows.Scan(&course.Id, &course.CreatedAt); err != nil {
+			return storage.Course{}, fmt.Errorf("scan error: %w", err)
+		}
+		return course, nil
+	}
+
+	return storage.Course{}, storage.ErrCourseNotFound
+}
+
+func (s *Storage) DeleteCourse(ctx context.Context, id int) error {
+	_, err := s.db.ExecContext(ctx, "DELETE FROM courses where id = $1", id)
+	if err != nil {
+		return fmt.Errorf("sql delete error: %w", err)
+	}
+
+	return nil
 }
